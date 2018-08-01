37b4b35c1efc0552386c689f7c33e293f23e15da
jellyfish77 <to.be.added#blah.com>
2018-08-01 18:53:53 +0800

    update ch04.p2p

--- src/ch04/p2p/QBorrower.java
+++ src/ch04/p2p/QBorrower.java
@@ -20,8 +20,12 @@ public class QBorrower {
 			// Create the JMS Session
 			qSession = qConnect.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
 			// Lookup the request and response queues
-			requestQ = (Queue) ctx.lookup(requestQueue);
+			System.out.print("Look up " + requestQueue);
+			//requestQ = (Queue) ctx.lookup(requestQueue);
+			System.out.println(" [OK]");
+			System.out.print("Look up " + responseQueue);
 			responseQ = (Queue) ctx.lookup(responseQueue);
+			//System.out.println(" [OK]");
 			// Now that setup is complete, start the Connection
 			qConnect.start();
 		} catch (JMSException jmse) {
