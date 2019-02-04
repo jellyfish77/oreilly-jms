d6e92b19edf7de7219bb0f852046f949c3010663
jellyfish77 <jellyfish77@gmx.com>
2019-02-04 16:12:02 +0800

    Add classes demonstrating store-and-forward

---
+++ src/ch07/guaranteedmessaging/JMSReceiver.java
@@ -0,0 +1,79 @@
+package ch07.guaranteedmessaging;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jms.*;
+import javax.naming.*;
+
+public class JMSReceiver implements MessageListener {
+	private List<String> messageBuffer = new ArrayList<String>();
+
+	public JMSReceiver() {
+		try {
+			Context ctx = new InitialContext();
+			QueueConnectionFactory factory = (QueueConnectionFactory) ctx.lookup("queueConnectionFactory");
+			QueueConnection connection = factory.createQueueConnection();
+			connection.start();
+			QueueSession session = connection.createQueueSession(false, Session.CLIENT_ACKNOWLEDGE);
+			Queue queue = (Queue) ctx.lookup("TEST.Q");
+			QueueReceiver receiver = session.createReceiver(queue);
+			receiver.setMessageListener(this);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public void onMessage(Message message) {
+		try {
+			if (message.propertyExists("SequenceMarker")) {
+				String marker = message.getStringProperty("SequenceMarker");
+				//if we are starting a message group, clear out the message buffer
+				if (marker.equals("START_SEQUENCE")) {
+					//since the messages are delivered and acknowledged as a group, any
+					//failures will result in the first sequence message being marked as
+					//being redelivered - we don't care about the others
+					if (message.getJMSRedelivered()) {
+						processCompensatingTransaction();
+					}
+					messageBuffer.clear();
+				}
+				//if we are ending the message group, process the message and
+				//acknowledge that all messages as having been delivered
+				if (marker.equals("END_SEQUENCE")) {
+					//process the message
+					System.out.println("Messages: ");
+					for (String msg : messageBuffer) {
+						System.out.println(msg);
+					}
+					//acknowledge that all messages have been received
+					message.acknowledge();
+				}
+			}
+			//save the message contents if it is a non-marker message
+			if (message instanceof TextMessage) {
+				TextMessage msg = (TextMessage) message;
+				processInterimMessage(msg.getText());
+			}
+			//wait for the next message
+			System.out.println("waiting for messages...");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	public void processCompensatingTransaction() {
+		//reverse the processing from the prior message set
+		messageBuffer.clear();
+	}
+
+	public void processInterimMessage(String msg) {
+		//process the interim message
+		messageBuffer.add(msg);
+	}
+
+	public static void main(String argv[]) {
+		new JMSReceiver();
+	}
+}
---
+++ src/ch07/guaranteedmessaging/JMSSender.java
@@ -0,0 +1,61 @@
+package ch07.guaranteedmessaging;
+
+import javax.jms.*;
+import javax.naming.*;
+
+public class JMSSender {
+	private QueueConnection connection = null;
+	private QueueSession session = null;
+	private QueueSender sender = null;
+
+	public static void main(String[] args) {
+		try {
+			JMSSender app = new JMSSender();
+			app.sendMessageGroup();
+			System.exit(0);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public JMSSender() {
+		try {
+			//connect to the jms provider and create the
+			//connection, session, and sender
+			Context ctx = new InitialContext();
+			QueueConnectionFactory factory = (QueueConnectionFactory) ctx.lookup("queueConnectionFactory");
+			connection = factory.createQueueConnection();
+			connection.start();
+			session = connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+			Queue queue = (Queue) ctx.lookup("TEST.Q");
+			sender = session.createSender(queue);
+		} catch (Exception jmse) {
+			jmse.printStackTrace();
+		}
+	}
+
+	public void sendMessageGroup() throws JMSException {
+		//send the messages as a group
+		sendSequenceMarker("START_SEQUENCE");
+		sendMessage("First Message");
+		sendMessage("Second Message");
+		sendMessage("Third Message");
+		sendSequenceMarker("END_SEQUENCE");
+		connection.close();
+	}
+
+	//send a simple text message within the group of messages
+	private void sendMessage(String text) throws JMSException {
+		TextMessage msg = session.createTextMessage(text);
+		msg.setStringProperty("JMSXGroupID", "GROUP1");
+		sender.send(msg);
+	}
+
+	//send an empty payload message containing the sequence marker
+	private void sendSequenceMarker(String marker) throws JMSException {
+		BytesMessage msg = session.createBytesMessage();
+		msg.setStringProperty("SequenceMarker", marker);
+		msg.setStringProperty("JMSXGroupID", "GROUP1");
+		sender.send(msg);
+	}
+}
